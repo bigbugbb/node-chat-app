2cd187906780c130b0ceb04b3fab8baf86175004
bigbugbb <bigbugbb@gmail.com>
2017-09-20 23:28:33 +0800

    Emit new Message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Bin',
-    text: 'Yup, this works for me.'
-  });
 });
 
 socket.on('disconnect', function() {

a27a6585762cd4dd68b0d3c13ecd0c0375e2a28a
bigbugbb <bigbugbb@gmail.com>
2017-09-24 03:36:49 +0800

    implement Users methods

--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,24 @@ const expect = require('expect');
 const { Users } = require('./users');
 
 describe('Users', () => {
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Bin Bo',
+      room: 'Node Course'
+    }, {
+      id: '2',
+      name: 'bigbug',
+      room: 'React Course'
+    }, {
+      id: '3',
+      name: 'Jen',
+      room: 'Node Course'
+    }];
+  });
 
   it('should add new user', () => {
     var users = new Users();
@@ -16,4 +34,45 @@ describe('Users', () => {
     expect(users.users).toEqual([user]);
   });
 
+  it('should remove a user', () => {
+    var userId = '1';
+    var user = users.removeUser(userId);
+
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('should not remove user', () => {
+    var userId = '99';
+    var user = users.removeUser(userId);
+
+    expect(user).toNotExist;
+    expect(users.users.length).toBe(3);
+  });
+
+  it('should find user', () => {
+    var userId = '2';
+    var user = users.getUser(userId);
+
+    expect(user.id).toBe(userId);
+  });
+
+  it('should not find user', () => {
+    var userId = '99';
+    var user = users.getUser(userId);
+
+    expect(user).toNotExist();
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node Course');
+
+    expect(userList).toEqual(['Bin Bo', 'Jen']);
+  });
+
+  it('should return names for react course', () => {
+    var userList = users.getUserList('React Course');
+
+    expect(userList).toEqual(['bigbug']);
+  });
 });
